1658b57e2fd91a06d8ebe298968b9bd46d8d1ec2
Lokeswarrao <lokeswarrao678@gmail.com>
2021-07-30 17:24:44 +0530

    third commit

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,9 @@
-
-
 var owl = $('.owl-carousel');
 owl.owlCarousel({
     items:3,
     lazyLoad: true,
     loop:true,
     nav:true,
-    navText : [
-        'prevIcon',
-        'nextIcon'
-    ],
     margin:10,
     autoplay:true,
     autoplayTimeout:2500,
