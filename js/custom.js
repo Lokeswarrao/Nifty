42c58e5fe3a7645f6f9d574a600cd0419fca3ce9
Lokeswarrao <lokeswarrao678@gmail.com>
2021-08-05 18:17:50 +0530

    added: about page

--- js/custom.js
+++ js/custom.js
@@ -7,5 +7,20 @@ owl.owlCarousel({
     margin:10,
     autoplay:true,
     autoplayTimeout:2500,
-    autoplayHoverPause:true
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
 });
